15f6ced14324bf88845236e4a855c9b067303c8e
Nabeel aron <35460291+Nabeelaron@users.noreply.github.com>
2024-03-14 23:53:48 +0530

    WA[AI]01: Add contribute doc and update test script

--- test/script.js
+++ test/script.js
@@ -1,2 +1,5 @@
-import func from "html_to_pdf";
-console.log(func());
+import { loadAndGenerateFile } from "template_html_to_pdf";
+import { fileURLToPath } from "url";
+import * as path from "path";
+
+loadAndGenerateFile(path.dirname(fileURLToPath(import.meta.url)));
